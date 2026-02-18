8247e0d55e19def5f71773479ecdb32c6c4d2544
Nikita <lisqxd@proton.me>
2026-02-18 17:54:32 +0100

    add vscode

---
+++ scripts/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  canvas: ["#1A1A23", "#1E1E29", "#22222F", "#2B2B3B", "#353447"],
+  ink: ["#5E5E6E", "#6C6C93", "#9C9CB5", "#E0E0EB"],
+  glow: ["#CECCFF0D", "#CECCFF16", "#B9B3FF22", "#A099FF2A", "#B9B3FF36"],
+  green: ["#3A8A6D", "#55BD97", "#78CCAD"],
+  cyan: ["#498F8F", "#78D0D0", "#A6DEDE"],
+  blue: ["#688ABA", "#85B0ED", "#A3C6F7"],
+  purple: ["#6D65A6", "#948BD9", "#B3ACE8"],
+  magenta: ["#9C5BA6", "#D08DD9", "#DEAEE6"],
+  pink: ["#944669", "#D06A97", "#E08BB1"],
+  orange: ["#B36766", "#DB8380", "#E09C9B"],
+  yellow: ["#9C785F", "#CE9F7D", "#D4B49D"],
+} as const;
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,11 @@
 import * as fs from "node:fs/promises";
 import { zedTheme } from "./theme-zed.ts";
+import { vscodeThemeDark } from "./theme-vscode.ts";
 
-// await fs.writeFile(
-//   import.meta.dirname + "/../vscode/themes/technicolor-dark.json",
-//   JSON.stringify(vscodeTheme, null, 2),
-// );
+await fs.writeFile(
+  import.meta.dirname + "/../vscode/themes/technicolor-dark.json",
+  JSON.stringify(vscodeThemeDark, null, 2),
+);
 
 await fs.writeFile(
   import.meta.dirname + "/../zed/themes/technicolor.json",
